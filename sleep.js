142fe962abdb65e58c30bdae2e60fd4a69c238c2
sergio <sergio>
2017-04-01 23:15:46 -0600

    calcula diferencia entre sleep/room y cama/sleep

--- sleep.js
+++ sleep.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
+	e = new Date();
 	a = 0;
     $("button").click(function(){
 		a++;
 		d = new Date();		
-		if (a==1) { $('#room').text(d.format("HH:MM"));	}
-		if (a==2) { $('#sleep').text(d.format("HH:MM")); }
-		if (a==3) { $('#cama').text(d.format("HH:MM"));	}
+		if (a==1) { 
+			$('#room').text(d.format("HH:MM:ss"));	
+			ant = d;
+		}
+		if (a==2) { 
+			miDif = d.getTime() - ant.getTime();
+			e.setTime(miDif);
+			$('#sleep').text(d.format("HH:MM:ss")+ '   ' + e.format("MM:ss") ); 
+			ant = d;
+		}
+		if (a==3) { 
+			miDif = d.getTime() - ant.getTime();
+			e.setTime(miDif);
+			$('#cama').text(d.format("HH:MM:ss")+ '   ' + e.format("MM:ss") ); 		
+		}
 		if (a==3) { a = 0}
     });
 });
